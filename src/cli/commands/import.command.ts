1bbe70ac4552a5e3662cb6c2af9ce971c4cb6708
anvts <worontsov.andrey@gmail.com>
2023-09-21 09:36:49 +0300

    Implements execute for import command

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,4 +1,5 @@
 import {Command} from './command.interface.js';
+import {TSVFileReader} from '../../shared/libs/file-reader/index.js';
 
 
 export class ImportCommand implements Command {
@@ -6,7 +7,20 @@ export class ImportCommand implements Command {
     return '--import';
   }
 
-  public async execute(..._parameters: string[]): Promise<void> {
-    // TODO: Reading a file
+  public async execute(...parameters: string[]): Promise<void> {
+    const [filename] = parameters;
+    const fileReader = new TSVFileReader(filename);
+
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch (error) {
+      if (!(error instanceof Error)) {
+        throw error;
+      }
+
+      console.error(`Can't import data from file: ${filename}`);
+      console.error(`Details: ${error.message}`);
+    }
   }
 }
