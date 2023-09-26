fa299c20649fd6680ee462515b1f5d7eb24208e8
anvts <worontsov.andrey@gmail.com>
2023-09-26 10:05:01 +0300

    Create helper functions

---
+++ src/shared/helpers/common.ts
@@ -0,0 +1,21 @@
+export function generateRandomNumber(min: number, max: number, fractionDigits = 0) {
+  return +((Math.random()) * (max - min) + min).toFixed(fractionDigits);
+}
+
+export function getRandomItemFromArray<T>(items: T[]): T {
+  return items[generateRandomNumber(0, items.length - 1)];
+}
+
+export function getRandomSliceFromArray<T>(items: T[]): T[] {
+  const startPosition = generateRandomNumber(0, items.length - 1);
+  const endPosition = startPosition + generateRandomNumber(startPosition, items.length);
+  return items.slice(startPosition, endPosition);
+}
+
+export function getRandomItemsFromArray<T>(items: T[], targetArrayLength: number): T[] {
+  const targetArray: T[] = [];
+  for (let i = 0; i < targetArrayLength; i++) {
+    targetArray.push(items[generateRandomNumber(0, items.length - 1)]);
+  }
+  return targetArray;
+}
